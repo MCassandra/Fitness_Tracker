38a28d7d5ece394b6ed046e0bb009d85f6bea394
Maria Cassandra Gugol <mcmgugol@gmail.com>
2021-10-23 09:57:51 -0700

    fixing workout.js

--- models/workout.js
+++ models/workout.js
@@ -3,28 +3,16 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-  day:{
-    type: Date,
-    default: ()=> new Date(),
-  },
-  Exercises: [{
+  day: Date,
+  Exercises:[ {
     type: String,
-    name: String,
+    name: {type:String, trim:true},
     duration: Number,
     weight: Number,
     reps: Number,
     sets: Number,
-    trim: true,
     required: "Enter a name for your workout"
   },
-  value: {
-    type: Number,
-    required: "Enter an amount"
-  },
-  date: {
-    type: Date,
-    default: Date.now
-  }
 ]
 });
 
